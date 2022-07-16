641fa0e90e7d4f4a475e85e399c1767661241038
arifnugrahaa <arifnugraha1243@gmail.com>
2022-07-16 22:19:13 +0700

    CRUD interaction meeting part 1

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,13 +2,49 @@
 
 namespace App\Http\Controllers;
 
+use App\User;
 use Illuminate\Http\Request;
 
 class AuthController extends Controller
 {
     public function store(Request $request)
     {
-        return 'It works';
+        $this->validate($request, [
+            'name' => 'required',
+            'email' => 'required|email',
+            'password' => 'required|min:5'
+        ]);
+
+         $name = $request->input('name');
+         $email = $request->input('email');
+         $password = $request->input('password');
+
+         $user = new User([
+            'name' => $name,
+            'email' => $email,
+            'password' => bcrypt($password)
+         ]);
+
+         if($user->save()) {
+            $user->signing = [
+                'href' => 'api/v1/user/signin',
+                'method' => 'POST',
+                'params' => 'email', 'password'
+            ];
+
+            $response = [
+                'msg' => 'User Created',
+                'user' => $user
+            ];
+
+            return response()->json($response, 201);
+         }
+
+         $response = [
+            'msg' => 'An error occurred'
+         ];
+
+         return response()->json($response, 404);
     }
 
     public function signing(Request $request)
--- app/Meeting.php
+++ app/Meeting.php
@@ -6,5 +6,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class Meeting extends Model
 {
-    //
+    protected $fillable = ['title', 'description', 'time'];
+
+    public function users()
+    {
+        $this->belongsToMany(User::class);
+    }
 }
