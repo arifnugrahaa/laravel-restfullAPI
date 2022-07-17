589b46aae8345a365e46d08a4e5363b8d8b3c8fc
arifnugrahaa <arifnugraha1243@gmail.com>
2022-07-17 21:14:24 +0700

    CRUD interaction meeting part 3

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\User;
+use App\Meeting;
 
 class RegisterController extends Controller
 {
@@ -34,7 +36,44 @@ class RegisterController extends Controller
      */
     public function store(Request $request)
     {
-        return 'It works';
+        $this->validate($request, [
+            'meeting_id' => 'required',
+            'user_id' => 'required'
+        ]);
+
+        $meeting_id = $request->input('meeting_id');
+        $user_id = $request->input('user_id');
+
+        $meeting = Meeting::findOrFail($meeting_id);
+        $user = User::findOrFail($user_id);
+
+        $message = [
+            'msg' => 'User is already registered for meeting',
+            'user' => $user,
+            'meeting' => $meeting,
+            'unregister' => [
+                'href' => 'api/v1/meeting/registration/' . $meeting->id,
+                'method' => 'DELETE',
+            ]
+        ];
+
+        if($meeting->users()->where('users.id', $user->id)->first()){
+            return response()->json($message, 404);
+        }
+
+        $user->meetings()->attach($meeting);
+
+        $response = [
+            'msg' => 'User Registered for meeting',
+            'meeting' => $meeting,
+            'user' => $user,
+            'unregister' => [
+                'href' => 'api/v1/meeting/registration/' . $meeting->id,
+                'method' => 'DELETE'
+            ]
+        ];
+
+        return response()->json($response, 201);
     }
 
     /**
@@ -79,6 +118,20 @@ class RegisterController extends Controller
      */
     public function destroy($id)
     {
-        return 'It works';
+        $meeting = Meeting::findOrFail($id);
+        $meeting->users()->detach();
+
+        $response = [
+            'msg' => 'User unregistered for meeting',
+            'meeting' => $meeting,
+            'user' => 'tbd',
+            'register' => [
+                'href' => 'api/v1/meeting/registration',
+                'method' => 'POST',
+                'params' => 'user_id, meeting_id'
+            ]
+        ];
+
+        return response()->json($response, 200);
     }
 }
